ee471fc3ffabbf05f75f59ab29025f565beeb413
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-18 09:05:07 +0300

    Updated Service component

--- Components/Services.jsx
+++ Components/Services.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import images from "next/image"
 
-const Services = () => {
-  return <div>Services</div>;
-};
+export default ({
+  setOpenProfile,
+  setCompleteModal,
+  setGetModal,
+  setStartModal,
+}) => {
+  const team = [
+    {
+      avatar: images.compShipment,
+    },
+    {
+      avatar: images.getShipment,
+    },
+    {
+      avatar: images.startShipment,
+    },
+    {
+      avatar: images.userProfile,
+    },
+    {
+      avatar: images.shipCount,
+    },
+    {
+      avatar: images.send,
+    }
+  ]
 
-export default Services;
+  const openModelBox = (text) => {
+    if (text === 1) {
+      setCompleteModal(true)
+    } else if (text === 2) {
+      setGetModal(true)
+    } else if (text ===3) {
+      setStartModal(true)
+    }else if (text === 4) {
+      setOpenProfile(true)
+    }
+  }
+  return (
+    <section className="py-0 pb-14">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="mt-12">
+          <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
+            {team.map((item, i) => (
+              <li key={i}>
+                <div 
+                onClick={() => openModelBox(i + 1)}
+                className="w-full h-60 sm:h-52 md:h-56"
+                >
+                  <Image
+                  src={item.avatar}
+                  className="w-full h-full object-cover object-center shadow-md
+                  rounded-xl" alt=""
+                   />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
