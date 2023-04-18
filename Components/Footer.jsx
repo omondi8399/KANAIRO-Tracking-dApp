7c03b5bd6e885958760f9daf0af313bfa39892a7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-18 08:39:03 +0300

    Updated Footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { Fot1, Fot2 } from "../Components/index"
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+export default () => {
+  const footerNavs = [
+    {
+      href: "javascript:void()",
+      name: "Terms"
+    },
+    {
+      href: "javascript:void()",
+      name: "License"
+    },
+    {
+      href: "javascript:void()",
+      name: "Privacy"
+    },
+    {
+      href: "javascript:void()",
+      name: "About us"
+    }
+  ]
 
-export default Footer;
+  return (
+    <footer className="pt-10">
+        <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+          <div className="justify-between sm:flex">
+            <div className="space-y-6">
+              <img src="https://www.floatui.com/logo.svg" alt="theLogo" className="w-32" />
+              <p className="max-w-md">
+                This is Kanairo Supply Chain Dapp Footer section
+              </p>
+              <ul className="flex flex-wrap items-center gap-4 text-sm sm:text-base">
+                {footerNavs.map((item, idx) => (
+                  <li className="text-gray-800 hover:text-gray-500 duration-150">
+                    <a key={idx} href={item.href}>
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mt-6">
+              <p className="text-gray-700 font-semibold">Get the app</p>
+              <div className="flex items-center gap-3 mt-3 sm:block">
+                <a href="javascript:void()">
+                  <Fot1 />
+                </a>
+                <a href="javascript:void()" className="mt-0 block sm:mt-3">
+                  <Fot2 />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 py-10 border-t md:text-center">
+            <p>©️ 2022 Rodgers Omondi. All rights reserved.</p>
+          </div>
+        </div>
+    </footer>
+  )
+}
+
