b0b37a1e212c09c7b584e2d0d6982ec5ef90d284
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-19 10:05:11 +0300

    Updated GetShipment component

--- Components/GetShipment.jsx
+++ Components/GetShipment.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useState } from "react"
 
-const GetShipment = () => {
-  return <div>GetShipment</div>;
-};
+export default ({ getModal, setGetModel, getShipment }) => {
+  const [index, setIndex] = useState(0)
+  const [singleShipmentData, setSingleShipmentData] = useState()
 
-export default GetShipment;
+  const getShipmentData = async () => {
+    const getData = await getShipment(index)
+    setSingleShipmentData(getData)
+    console.log(getData)
+  }
+  console.log(singleShipmentData)
+
+  const convertTime = (time) => {
+      const newTime = new Date(time)
+      const dataTime = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-dog"
+      })
+  }
+}
