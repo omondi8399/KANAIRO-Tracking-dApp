7a6ce866523eb5c419fb512cf18d7e36ada35c23
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-19 15:52:18 +0300

    Updated StartShipment component

--- Components/StartShipment.jsx
+++ Components/StartShipment.jsx
@@ -44,8 +44,8 @@ export default ({ startModal, setStartModal, startShipment }) => {
               </div>
               <div className="relative mt-3">
                 <input
-                  type="text"
-                  placeholder="Id"
+                  type="number"
+                  placeholder="ID"
                   className="w-full pl-5 pr-3 py-2 text-gray-500 bg-transparent
                   outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                   onChange={(e) => setGetProduct({
